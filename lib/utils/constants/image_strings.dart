52bf0eb8d2f0fc559e16bfab21d164e96a8c5b84
Ahsan Khan <ahsankhancodes@gmail.com>
2024-05-09 03:11:21 +0500

    Useful functionalities implemented in lib/utils.

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,7 @@
+class MyImages{
+  static const String lightAppLogo = 'assets/logos/my-store-splash-logo-black.png';
+  static const String darkAppLogo = 'assets/logos/my-store-splash-logo-black.png';
+
+  static const String google = 'assets/logos/google-icon.png';
+  static const String facebook = 'assets/logos/facebook-icon.png';
+}
