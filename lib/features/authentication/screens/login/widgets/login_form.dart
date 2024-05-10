559e2b5714abd20d025301ba1516d1a5bc00e342
Ahsan Khan <ahsankhancodes@gmail.com>
2024-05-11 02:00:21 +0500

    Bottom Navigaton Bar added.

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fyp_store/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:fyp_store/features/authentication/screens/signup/signup.dart';
+import 'package:fyp_store/navigation_menu.dart';
 import 'package:fyp_store/utils/constants/sizes.dart';
 import 'package:fyp_store/utils/constants/text_strings.dart';
 import 'package:get/get.dart';
@@ -68,7 +69,7 @@ class MyLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const NavigationMenu()),
                 child: const Text(MyTexts.signIn),
               ),
             ),
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:fyp_store/features/shop/screens/home/widgets/home.dart';
+import 'package:fyp_store/utils/helpers/helper_functions.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import 'utils/constants/colors.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = MyHelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          backgroundColor: darkMode ? MyColors.black : MyColors.white,
+          indicatorColor: darkMode ? MyColors.white.withOpacity(0.1) : MyColors.black.withOpacity(0.1),
+
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Shop'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController{
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.purple,),
+    Container(color: Colors.orange,),
+    Container(color: Colors.blue,),
+  ];
+}
